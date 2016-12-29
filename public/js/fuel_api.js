e40972d5c1df88383c2a3f31c1e79f511382379d
paschalidis <msf7415012@teilar.gr>
2016-12-29 15:08:29 +0200

    refactoring javascript files

---
+++ public/js/fuel_api.js
@@ -0,0 +1,231 @@
+// Login Values
+var api_token = "";
+var username = "";
+var userGasStation = "";
+
+//Settings
+var defaultFuelTypeID = '1';
+
+$( document ).ready(function() {
+    loginSubmit();
+    registerSubmit();
+});
+
+function getGasStations() {
+    var gasStationsIDs = [];
+    var mapBounds = map.getBounds();
+    var latIN = mapBounds.f.f + ',' + mapBounds.f.b;
+    var lngIN = mapBounds.b.b + ',' + mapBounds.b.f;
+
+    var _data = {'fields': 'gasStationID,gasStationLat,gasStationLong,fuelCompNormalName',
+        'gasStationLat_BETWEEN' : latIN,
+        'gasStationLong_BETWEEN' : lngIN};
+
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/gasstations/",
+        data: _data,
+        success: function(response){
+            $('#gasStationsNumber').text(response.length);
+            var largeInfowindow = new google.maps.InfoWindow();
+            $.each(response, function(i, item) {
+                gasStationsIDs.push(item.gasStationID);
+                var gasStationPosition = new google.maps.LatLng(item.gasStationLat, item.gasStationLong);
+
+                var myMarker = new google.maps.Marker({
+                    map: map,
+                    position: gasStationPosition,
+                    title: item.fuelCompNormalName
+                });
+
+                markers.push(myMarker);
+                myMarker.addListener('click', function() {
+                    populateInfoWindow(this, largeInfowindow);
+                });
+
+            });
+
+            getPriceDataAnalytics(gasStationsIDs);
+        }
+    });
+
+}
+
+function getPriceDataAnalytics(gasStationsIDs) {
+
+    if(gasStationsIDs.length <= 0){
+        $('#minPrice').text('0');
+        $('#avgPrice').text('0');
+        $('#maxPrice').text('0');
+        return;
+    }
+
+    var _data = {gasStationID : gasStationsIDs,
+        'max' : 'fuelPrice',
+        'min' : 'fuelPrice',
+        'avg' : 'fuelPrice',
+        'fuelTypeID' : defaultFuelTypeID};
+
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/pricedata/",
+        data: _data,
+        success: function(response){
+            $('#minPrice').text(response[0].min_fuelPrice);
+            $('#avgPrice').text(response[0].avg_fuelPrice);
+            $('#maxPrice').text(response[0].max_fuelPrice);
+        }
+    });
+}
+
+function loginSubmit() {
+    $("#loginSubmit").click(function(event){
+
+        event.preventDefault();// using this page stop being refreshing
+        $('#loginError').empty();
+        $('.help-block').remove();
+        $('.has-error').removeClass('has-error');
+        $.ajax({
+            type: "POST",
+            url: "https://fuel.local/api/v1/login/",
+            data: $('#loginForm').serialize(),
+            success: function(response){
+                api_token = response.api_token;
+                username = $("#loginUsername").val();
+
+                getOrders();
+                getUserGasStation();
+                $('#loginModal').modal('hide');
+                $.notify({
+                    // options
+                    message: "Successfully login"
+                },{
+                    // settings
+                    type: 'success',
+                    placement: {
+                        from: "top",
+                        align: "center"
+                    },
+                });
+            },
+            error: function(response){
+                $('#loginError').append(response.responseJSON.message);
+                $.each(response.responseJSON, function(inputName, errorMessage){
+                    $("#loginForm input[name=" + inputName + "]").
+                    after('<span class="help-block">' + errorMessage + '</span>')
+                        .parent('div').addClass('has-error');
+                });
+            }
+        });
+    });
+}
+
+function registerSubmit() {
+    $("#registerSubmit").click(function(event){
+
+        event.preventDefault();// using this page stop being refreshing
+        $('.help-block').remove();
+        $('#registerError').empty();
+        $('.has-error').removeClass('has-error');
+        $.ajax({
+            type: "POST",
+            url: "https://fuel.local/api/v1/register/",
+            data: $('#registerForm').serialize(),
+            success: function(response){
+                $('#registerModal').modal('hide');
+                $.notify({
+                    // options
+                    message: response.message
+                },{
+                    // settings
+                    type: 'success',
+                    placement: {
+                        from: "top",
+                        align: "center"
+                    }
+                });
+            },
+            error: function(response){
+                $('#registerError').append(response.responseJSON.message);
+                $.each(response.responseJSON, function(inputName, errorMessage){
+                    $("#registerForm input[name=" + inputName + "]").
+                    after('<span class="help-block">' + errorMessage + '</span>')
+                        .parent('div').addClass('has-error');
+                });
+            }
+        });
+    });
+}
+
+function getOrders() {
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/orders/",
+        data: {"api_token": api_token, "owner" : username},
+        success: function(response){
+            $('#ordersList').show();
+            $('#ordersNumber').append(response.length);
+
+            $.each(response, function(i, item) {
+                var  row = i + 1;
+                $('#ordersTable').append('<tr><th scope="row">' + row + '</th>' +
+                    '<td> ' + item.client + ' </td>' +
+                    '<td> ' + item.fuelName + ' </td>' +
+                    '<td> ' + item.fuelPrice + ' </td>' +
+                    '<td> ' + item.quantity + ' </td>' +
+                    '<td> '+item.create_time+' </td></tr>');
+            });
+
+            $.notify({
+                // options
+                message: response.message
+            },{
+                // settings
+                type: 'success',
+                placement: {
+                    from: "top",
+                    align: "center"
+                }
+            });
+        }
+    });
+}
+
+function getUserGasStation() {
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/gasstations/",
+        data: {"fields": "gasStationID", "username" : username},
+        success: function(response){
+            $('#updateFuelData').show();
+            userGasStation = response[0].gasStationID;
+        }
+    });
+}
+
+$('#priceDataModal').on('show.bs.modal', function(event) {
+
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/gasstations/" + userGasStation + "/pricedata/",
+        success: function(response){
+
+            $.each(response, function(i, item) {
+                var  row = i + 1;
+                var  premium = "Yes";
+                if(item.isPremium == 0){
+                    premium = "No";
+                }
+
+
+                $('#priceDataTable').append('<tr><th scope="row">' + row + '</th>' +
+                    '<td> ' + item.fuelNormalName + ' </td>' +
+                    '<td> ' + item.fuelName + ' </td>' +
+                    '<td> ' + item.fuelPrice + ' </td>' +
+                    '<td> ' + item.dateUpdated + ' </td>' +
+                    '<td> ' + premium + ' </td></tr>');
+            });
+        }
+    });
+
+});
---
+++ public/js/map_api.js
@@ -0,0 +1,158 @@
+//Map Values
+var map = null;
+var markers = []; // Create a new blank array for all the listing markers.
+
+document.getElementById('zoom-to-area').addEventListener('click', function() {
+    zoomToArea();
+});
+
+$('#zoom-to-area-text').keypress(function (e) {
+    if (e.which == 13) {
+        $("#zoom-to-area").click();
+        return false;    //<---- Add this line
+    }
+});
+
+//Initialized JavaScript functions to load the map
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    //Used in callback
+
+    var Greece = new google.maps.LatLng(37.840834994646166, 23.74952344409178);
+    //New map Instance
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: Greece,
+        zoom: 6,
+        mapTypeControl: false
+    });
+
+    map.addListener('idle', function() {
+        // 3 seconds after the center of the map has changed, pan back to the
+        // marker.
+        cleanMarkers();
+        // window.setTimeout(function() {
+        getGasStations();
+        //}, 1000);
+    });
+
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick',function(){
+            infowindow.setMarker(null);
+        });
+    }
+}
+
+function cleanMarkers() {
+    for (var i=0; i<markers.length; i++){
+        markers[i].setMap(null);
+    }
+    //άδειασμα και του πίνακα με τα references
+    markers=[];
+}
+
+// This function takes the input value in the find nearby area text input
+// locates it, and then zooms into that area. This is so that the user can
+// show all listings, then decide to focus on one area of the map.
+function zoomToArea() {
+    // Initialize the geocoder.
+    var geocoder = new google.maps.Geocoder();
+    // Get the address or place that the user entered.
+    var address = document.getElementById('zoom-to-area-text').value;
+    // Make sure the address isn't blank.
+    if (address == '') {
+        $.notify({
+            // options
+            message: 'You must enter an area, or address.'
+        },{
+            // settings
+            type: 'danger',
+            placement: {
+                from: "top",
+                align: "center"
+            }
+        });
+    } else {
+        // Geocode the address/area entered to get the center. Then, center the map
+        // on it and zoom in
+        geocoder.geocode(
+            { address: address
+            }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    map.setZoom(14);
+                } else {
+                    $.notify({
+                        // options
+                        message: 'We could not find that location - try entering a more' +
+                        ' specific place.'
+                    },{
+                        // settings
+                        type: 'danger',
+                        placement: {
+                            from: "top",
+                            align: "center"
+                        }
+                    });
+                }
+            });
+    }
+}
+
+function initMap2() {
+    var initCenter = new google.maps.LatLng(39.6315214,22.4464073);
+    var mapOptions = {//create the map object
+        center:initCenter,
+        zoom:7,
+        scaleControl: true,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position:google.maps.ControlPosition.TOP_LEFT,
+        },
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP },
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+
+    //New map Instance
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    $.ajax({
+        type: "GET",
+        url: "https://fuel.local/api/v1/gasstations/",
+        success: function(response){
+
+            $.each(response, function(i, item) {
+
+                console.log(item.gasStationLat);
+                var myPosition = new google.maps.LatLng(item.gasStationLat, item.gasStationLong);
+
+                var myMarker = new google.maps.Marker({
+                    position: myPosition,
+                    map: map,
+                });
+
+                markers.push(myMarker);
+            });
+        }
+    });
+}
